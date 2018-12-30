1d5932871886591dcc982323e22b72eff5cc9d20
Gabriel Jony Borges Silva <gabjony@gmail.com>
2018-12-30 13:45:45 -0300

    gerado os components

--- src/main/resources/templates/projetoCadastro-client/src/app/app.module.ts
+++ src/main/resources/templates/projetoCadastro-client/src/app/app.module.ts
@@ -2,10 +2,18 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
+import { ClienteCreateComponent } from './cliente-create/cliente-create.component';
+import { ClienteDetailsComponent } from './cliente-details/cliente-details.component';
+import { ClienteListComponent } from './cliente-list/cliente-list.component';
+import { ClienteSearchComponent } from './cliente-search/cliente-search.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    ClienteCreateComponent,
+    ClienteDetailsComponent,
+    ClienteListComponent,
+    ClienteSearchComponent
   ],
   imports: [
     BrowserModule
