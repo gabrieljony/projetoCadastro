72965161537eb24b982dd8272e7fe8aa2eb39a8d
Gabriel Jony Borges Silva <gabjony@gmail.com>
2018-12-30 14:42:15 -0300

    detalhes do cliente

--- src/main/resources/templates/projetoCadastro-client/src/app/cliente-details/cliente-details.component.ts
+++ src/main/resources/templates/projetoCadastro-client/src/app/cliente-details/cliente-details.component.ts
@@ -1,4 +1,8 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, Input } from '@angular/core';
+
+import { ClienteListComponent } from './../cliente-list/cliente-list.component';
+import { ClienteService } from './../cliente.service';
+import { Cliente } from '../cliente';
 
 @Component({
   selector: 'app-cliente-details',
@@ -7,9 +11,33 @@ import { Component, OnInit } from '@angular/core';
 })
 export class ClienteDetailsComponent implements OnInit {
 
-  constructor() { }
+  @Input() 
+  cliente: Cliente;
+
+  constructor(private servico: ClienteService, private listaClientes: ClienteListComponent) { }
 
   ngOnInit() {
+  }
+
+  updateAtivo(isActive: boolean) {
+    this.servico.updateCliente(this.cliente.id,
+      { nome: this.cliente.nome, cpf: this.cliente.cpf, ativo: isAtivo })
+      .subscribe(
+        data => {
+          console.log(data);
+          this.cliente = data as Cliente;
+        },
+        error => console.log(error));
+  }
+ 
+  deleteCliente() {
+    this.servico.deleteCliente(this.cliente.id)
+      .subscribe(
+        data => {
+          console.log(data);
+          this.listaClientes.reloadData();
+        },
+        error => console.log(error));
   }
 
 }
