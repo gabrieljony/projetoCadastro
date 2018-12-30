d6fc9ec77158aac6c113a903381b2f9b4b8642bf
Gabriel Jony Borges Silva <gabjony@gmail.com>
2018-12-30 13:14:15 -0300

    backend da aplicacao usando Spring Boot v2.1.1
    Spring Boot com a v2.1.1
    Maven
    Java v8
    dependencies Web, JPA, PostegreSQL

---
+++ src/main/java/com/example/gabrieljony/projetoCadastroapi/ProjetoCadastroApiApplication.java
@@ -0,0 +1,14 @@
+package com.example.gabrieljony.projetoCadastroapi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ProjetoCadastroApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ProjetoCadastroApiApplication.class, args);
+	}
+
+}
+
---
+++ src/test/java/com/example/gabrieljony/projetoCadastroapi/ProjetoCadastroApiApplicationTests.java
@@ -0,0 +1,17 @@
+package com.example.gabrieljony.projetoCadastroapi;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class ProjetoCadastroApiApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
+
